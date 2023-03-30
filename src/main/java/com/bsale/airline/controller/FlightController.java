1c8912b57bc8bad0756bc98e5174267e77fbe21e
xyeison2010 <leonardohuacahidalgo@gmail.com>
2023-03-30 18:03:36 -0500

    creacion del Controller para las peticiones Rest

---
+++ src/main/java/com/bsale/airline/controller/FlightController.java
@@ -0,0 +1,41 @@
+package com.bsale.airline.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bsale.airline.dto.FlightDTO;
+
+
+import com.bsale.airline.service.FlightService;
+
+@RestController
+@CrossOrigin  //Esta anotacion permite llamadas desde cualquier origen, 
+@RequestMapping("springairline/api/v1/flights")
+public class FlightController {
+
+	@Autowired
+	FlightService flightService;
+
+	//for unit tests
+	public FlightController(FlightService flightService) {
+		this.flightService = flightService;
+	}
+
+
+
+	@GetMapping("/{id}/passengers")
+	public ResponseEntity<Object> listFlightWithPassengerList(@PathVariable Long id) {
+
+		FlightDTO flightDTO = flightService.findByIdWithPassengersList(id);
+
+		return ResponseEntity.ok(flightDTO);
+
+	}
+
+}
